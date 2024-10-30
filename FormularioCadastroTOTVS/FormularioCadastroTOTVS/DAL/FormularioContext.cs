e8adfa88618d449bfc5a7027027be8ceefeeb105
Marlon Oliveira Santos <marlon.oliveira34@hotmail.com>
2024-10-30 11:39:26 -0300

    Segundo Commit

---
+++ FormularioCadastroTOTVS/FormularioCadastroTOTVS/DAL/FormularioContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FormularioCadastroTOTVS.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FormularioCadastroTOTVS.DAL
+{
+    public class FormularioContext : DbContext
+    {
+
+        public FormularioContext() :base("FormularioContext")
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        //Para não deixar a tabela no plural dentro do banco de dados
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
